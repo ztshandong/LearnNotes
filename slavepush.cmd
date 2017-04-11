5028739dcd7ae78da76ba8e8fbe03732585c3292
ztshandong <ztshandong@gmail.com>
2017-04-11 14:47:24 +0800

    init

---
+++ slavepush.cmd
@@ -0,0 +1,4 @@
+git push github slaveBranch
+git push osc slaveBranch
+git push gitlab slaveBranch
+git push bitbucket slaveBranch
---
+++ thirdpush.cmd
@@ -0,0 +1,4 @@
+git push github thirdBranch
+git push osc thirdBranch
+git push gitlab thirdBranch
+git push bitbucket thirdBranch
