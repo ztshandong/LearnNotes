66966bc1cc61aed4bcb7eec95e377078c6efcd13
ztshandong <ztshandong@gmail.com>
2017-04-11 18:02:56 +0800

    add del

---
+++ delslave.cmd
@@ -0,0 +1,4 @@
+git push github --delete slaveBranch
+git push osc --delete slaveBranch
+git push gitlab --delete slaveBranch
+git push bitbucket --delete slaveBranch
---
+++ delthird.cmd
@@ -0,0 +1,4 @@
+git push github --delete thirdBranch
+git push osc --delete thirdBranch
+git push gitlab --delete thirdBranch
+git push bitbucket --delete thirdBranch
