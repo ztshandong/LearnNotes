44585555f4d926c2b5d947eeccbc38b44e1b593a
ztshandong <ztshandong@gmail.com>
2017-04-11 16:04:15 +0800

    all

---
+++ m2s.cmd
@@ -0,0 +1,4 @@
+git push github master:slaveBranch
+git push osc master:slaveBranch
+git push gitlab master:slaveBranch
+git push bitbucket master:slaveBranch
---
+++ m2t.cmd
@@ -0,0 +1,4 @@
+git push github master:thirdBranch
+git push osc master:thirdBranch
+git push gitlab master:thirdBranch
+git push bitbucket master:thirdBranch
